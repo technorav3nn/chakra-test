877c9ec4ba8b6c1c026d6b9a375c71ea792e9925
technorav3nn <65562685+technorav3nn@users.noreply.github.com>
2021-10-18 12:39:49 -0400

    Initial commit

---
+++ client/src/ColorModeSwitcher.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import {
+  useColorMode,
+  useColorModeValue,
+  IconButton,
+  IconButtonProps,
+} from "@chakra-ui/react";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { Tooltip } from "@material-ui/core";
+
+type ColorModeSwitcherProps = Omit<IconButtonProps, "aria-label">;
+
+export const ColorModeSwitcher: React.FC<ColorModeSwitcherProps> = (props) => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const text = useColorModeValue("dark", "light");
+  const SwitchIcon = useColorModeValue(FaMoon, FaSun);
+
+  return (
+    <Tooltip
+      title={
+        colorMode === "light" ? "Switch to night mode" : "Switch to light mode"
+      }
+    >
+      <IconButton
+        size="md"
+        fontSize="lg"
+        color="current"
+        marginLeft="2"
+        onClick={toggleColorMode}
+        icon={<SwitchIcon />}
+        aria-label={`Switch to ${text} mode`}
+        {...props}
+      />
+    </Tooltip>
+  );
+};
